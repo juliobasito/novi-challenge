f47258ad0d21bdafa148056b33bb0cc582d609a7
Vincent BERAUD <vincent.beraud@y-nov.com>
2015-11-24 09:18:03 +0100

    Recuperation prenom nom utilisateur avec le mail

--- views/profil/index.php
+++ views/profil/index.php
@@ -1,3 +1,6 @@
 <?php 
-var_dump($_SESSION);
+$prenom= explode(".", $_SESSION['mail']);
+$rest = $prenom[1];
+$nom = explode("@", $rest);
+echo "Bienvenue ".ucfirst($prenom[0])." ".strtoupper($nom[0]);
 ?>
