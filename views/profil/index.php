62e6826ae4db4081aabbcd5f6f939f7c7945d882
Jules Basse-Cathalinat <jules.basse-cathalinat@y-nov.com>
2015-11-24 10:34:50 +0100

    calendar

--- views/profil/index.php
+++ views/profil/index.php
@@ -6,3 +6,32 @@ echo "Bienvenue ".ucfirst($prenom[0])." ".strtoupper($nom[0]);
 
 var_dump($_SESSION);
 ?>
+<div class="container">
+    <hr>
+	<div id="calendar"></div>
+</div>
+
+<script>
+$.getScript('http://arshaw.com/js/fullcalendar-1.6.4/fullcalendar/fullcalendar.min.js',function(){
+  
+  var date = new Date();
+  var d = date.getDate();
+  var m = date.getMonth();
+  var y = date.getFullYear();
+  
+  $('#calendar').fullCalendar({
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    editable: true,
+    events: [
+      {
+        title: 'All Day Event',
+        start: new Date(y, m, 1)
+      }
+    ]
+  });
+})
+</script>
