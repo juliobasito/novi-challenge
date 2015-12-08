8d6f77ff09a476908ec4a39c82274bf339688f83
Vincent BERAUD <vincent.beraud@y-nov.com>
2015-12-08 15:02:20 +0100

    je pull

--- views/profil/index.php
+++ views/profil/index.php
@@ -2,6 +2,10 @@
 
 echo "Bienvenue ".$profilName." élève de ".$class['className'];
 
+$compteur=0;
+foreach ($task as $key => $value) {
+  echo "Projet de ".$value['subjectName'];
+}
 
 var_dump($task);
 ?>
